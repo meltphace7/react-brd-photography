a6a448cb64ce851f313fcc583d15ccb88c85c505
meltphace7 <brd9326@protonmail.com>
2022-07-14 12:06:16 -0700

    completed

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,65 +1,71 @@
 import React from "react";
 import classes from "./Portfolio.module.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Portfolio = () => {
   return (
     <div className={classes.portfolio}>
       <h1>PORTFOLIO</h1>
       <div className={classes.grid}>
-        <div className={`${classes.grid__item} ${classes["grid__item--1"]}`}>
-            <div
-              className={`${classes["grid__item--image"]} ${classes["grid__item--image--1"]}`}
-            ></div>
-          <Link to="/washington">
-            <h2 className={classes["grid__item--text"]}>WASHINGTON</h2>
-          </Link>
-        </div>
+        <Link
+          to="/washington"
+          className={`${classes.grid__item} ${classes["grid__item--1"]}`}
+        >
+          <div
+            className={`${classes["grid__item--image"]} ${classes["grid__item--image--1"]}`}
+          ></div>
+          <h2 className={classes["grid__item--text"]}>WASHINGTON</h2>
+        </Link>
 
-        <div className={`${classes.grid__item} ${classes["grid__item--2"]}`}>
+        <Link
+          to="/oregon"
+          className={`${classes.grid__item} ${classes["grid__item--2"]}`}
+        >
           <div
             className={`${classes["grid__item--image"]} ${classes["grid__item--image--2"]}`}
           ></div>
-          <Link to="/oregon">
-            <h2 className={classes["grid__item--text"]}>OREGON</h2>
-          </Link>
-        </div>
+          <h2 className={classes["grid__item--text"]}>OREGON</h2>
+        </Link>
 
-        <div className={`${classes.grid__item} ${classes["grid__item--3"]}`}>
+        <Link
+          to="/california"
+          className={`${classes.grid__item} ${classes["grid__item--3"]}`}
+        >
           <div
             className={`${classes["grid__item--image"]} ${classes["grid__item--image--3"]}`}
           ></div>
-          <Link to="/california">
-            <h2 className={classes["grid__item--text"]}>CALIFORNIA</h2>
-          </Link>
-        </div>
+          <h2 className={classes["grid__item--text"]}>CALIFORNIA</h2>
+        </Link>
 
-        <div className={`${classes.grid__item} ${classes["grid__item--4"]}`}>
+        <Link
+          to="/idaho"
+          className={`${classes.grid__item} ${classes["grid__item--4"]}`}
+        >
           <div
             className={`${classes["grid__item--image"]} ${classes["grid__item--image--4"]}`}
           ></div>
-          <Link to="/idaho">
-            <h2 className={classes["grid__item--text"]}>IDAHO</h2>
-          </Link>
-        </div>
+          <h2 className={classes["grid__item--text"]}>IDAHO</h2>
+        </Link>
 
-        <div className={`${classes.grid__item} ${classes["grid__item--5"]}`}>
+        <Link
+          to="/wyoming"
+          className={`${classes.grid__item} ${classes["grid__item--5"]}`}
+        >
           <div
             className={`${classes["grid__item--image"]} ${classes["grid__item--image--5"]}`}
           ></div>
-          <Link to="/wyoming">
-            <h2 className={classes["grid__item--text"]}>WYOMING</h2>
-          </Link>
-        </div>
+          <h2 className={classes["grid__item--text"]}>WYOMING</h2>
+        </Link>
 
-        <div className={`${classes.grid__item} ${classes["grid__item--6"]}`}>
+        <Link
+          to="/montana"
+          className={`${classes.grid__item} ${classes["grid__item--6"]}`}
+        >
           <div
             className={`${classes["grid__item--image"]} ${classes["grid__item--image--6"]}`}
           ></div>
-          <Link to="/montana">
-            <h2 className={classes["grid__item--text"]}>MONTANA</h2>
-          </Link>
-        </div>
+          <h2 className={classes["grid__item--text"]}>MONTANA</h2>
+        </Link>
       </div>
     </div>
   );
