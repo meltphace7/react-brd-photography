e4ea79733a7168005c6cb947b547091b158fa3db
meltphace7 <brd9326@protonmail.com>
2022-07-13 17:31:14 -0700

    added dots

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,14 +1,14 @@
 import React from "react";
 import classes from "./ImageSlider.module.css";
 import { useState, useEffect } from "react";
-import { HiOutlineArrowSmLeft } from 'react-icons/hi'
+import { HiOutlineArrowSmLeft } from "react-icons/hi";
 import { HiOutlineArrowSmRight } from "react-icons/hi";
 import { useInView } from "react-intersection-observer";
 
 const ImageSlider = (props) => {
   const [currentSlide, setCurrentSlide] = useState(1);
   const [controlsRevealed, setControlsRevealed] = useState(false);
-  const length = 8;
+  const length = 10;
 
   const nextSlideHandler = () => {
     setCurrentSlide(currentSlide === length ? 1 : currentSlide + 1);
@@ -18,28 +18,66 @@ const ImageSlider = (props) => {
     setCurrentSlide(currentSlide === 1 ? length : currentSlide - 1);
   };
 
+  const dotOneHandler = () => {
+    setCurrentSlide(1);
+  };
+
+  const dotTwoHandler = () => {
+    setCurrentSlide(2);
+  };
+
+  const dotThreeHandler = () => {
+    setCurrentSlide(3);
+  };
+
+  const dotFourHandler = () => {
+    setCurrentSlide(4);
+  };
+
+  const dotFiveHandler = () => {
+    setCurrentSlide(5);
+  };
+
+  const dotSixHandler = () => {
+    setCurrentSlide(6);
+  };
+
+  const dotSevenHandler = () => {
+    setCurrentSlide(7);
+  };
+
+  const dotEightHandler = () => {
+    setCurrentSlide(8);
+  };
+
+  const dotNineHandler = () => {
+    setCurrentSlide(9);
+  };
+
+  const dotTenHandler = () => {
+    setCurrentSlide(10);
+  };
+
   const options = { root: null, threshold: 0.2 };
 
   const { ref: controlsRef, inView: controlsVisible } = useInView(options);
 
-    useEffect(() => {
-      if (controlsVisible) {
-        setControlsRevealed(true);
-      }
-    }, [controlsVisible]);
+  useEffect(() => {
+    if (controlsVisible) {
+      setControlsRevealed(true);
+    }
+  }, [controlsVisible]);
 
   console.log(currentSlide);
 
   return (
     <div className={classes["slider-container"]}>
       <div className={classes.slider}>
-
         <div
           className={`${classes["header-slide"]} ${
-            classes[`header-slide-${currentSlide}  `]
+            classes[`header-slide-${currentSlide}`]
           }`}
         ></div>
-        
       </div>
       <div
         ref={controlsRef}
@@ -47,10 +85,98 @@ const ImageSlider = (props) => {
           !controlsRevealed ? classes["slider-controls--hidden"] : ""
         }`}
       >
-        <button className={classes["prev-slide"]} onClick={prevSlideHandler}>
+        <button
+          className={`${classes["prev-slide"]} ${classes["slider-control-button"]}`}
+          onClick={prevSlideHandler}
+        >
           <HiOutlineArrowSmLeft size="30px" />
         </button>
-        <button className={classes["next-slide"]} onClick={nextSlideHandler}>
+        <div className={classes["dots-container"]}>
+          <button
+            className={
+              currentSlide === 1
+                ? `${classes.dot} ${classes.active}`
+                : classes.dot
+            }
+            onClick={dotOneHandler}
+          ></button>
+          <button
+            className={
+              currentSlide === 2
+                ? `${classes.dot} ${classes.active}`
+                : classes.dot
+            }
+            onClick={dotTwoHandler}
+          ></button>
+          <button
+            className={
+              currentSlide === 3
+                ? `${classes.dot} ${classes.active}`
+                : classes.dot
+            }
+            onClick={dotThreeHandler}
+          ></button>
+          <button
+            className={
+              currentSlide === 4
+                ? `${classes.dot} ${classes.active}`
+                : classes.dot
+            }
+            onClick={dotFourHandler}
+          ></button>
+          <button
+            className={
+              currentSlide === 5
+                ? `${classes.dot} ${classes.active}`
+                : classes.dot
+            }
+            onClick={dotFiveHandler}
+          ></button>
+          <button
+            className={
+              currentSlide === 6
+                ? `${classes.dot} ${classes.active}`
+                : classes.dot
+            }
+            onClick={dotSixHandler}
+          ></button>
+          <button
+            className={
+              currentSlide === 7
+                ? `${classes.dot} ${classes.active}`
+                : classes.dot
+            }
+            onClick={dotSevenHandler}
+          ></button>
+          <button
+            className={
+              currentSlide === 8
+                ? `${classes.dot} ${classes.active}`
+                : classes.dot
+            }
+            onClick={dotEightHandler}
+          ></button>
+          <button
+            className={
+              currentSlide === 9
+                ? `${classes.dot} ${classes.active}`
+                : classes.dot
+            }
+            onClick={dotNineHandler}
+          ></button>
+          <button
+            className={
+              currentSlide === 10
+                ? `${classes.dot} ${classes.active}`
+                : classes.dot
+            }
+            onClick={dotTenHandler}
+          ></button>
+        </div>
+        <button
+          className={`${classes["next-slide"]} ${classes["slider-control-button"]}`}
+          onClick={nextSlideHandler}
+        >
           <HiOutlineArrowSmRight size="30px" />
         </button>
       </div>
@@ -59,33 +185,3 @@ const ImageSlider = (props) => {
 };
 
 export default ImageSlider;
-
-//  const slides = [
-//     <div
-//       className={`${classes["header-slide"]} ${classes[`header-slide-1`]}`}
-//     ></div>,
-//     <div
-//       className={`${classes["header-slide"]} ${classes[`header-slide-2`]}`}
-//     ></div>,
-//     <div
-//       className={`${classes["header-slide"]} ${classes[`header-slide-3`]}`}
-//     ></div>,
-//     <div
-//       className={`${classes["header-slide"]} ${classes[`header-slide-4`]}`}
-//     ></div>,
-//     <div
-//       className={`${classes["header-slide"]} ${classes[`header-slide-5`]}`}
-//     ></div>,
-//     <div
-//       className={`${classes["header-slide"]} ${classes[`header-slide-6`]}`}
-//     ></div>,
-//     <div
-//       className={`${classes["header-slide"]} ${classes[`header-slide-7`]}`}
-//     ></div>,
-//     <div
-//       className={`${classes["header-slide"]} ${classes[`header-slide-8`]}`}
-//     ></div>,
-//     <div
-//       className={`${classes["header-slide"]} ${classes[`header-slide-9`]}`}
-//     ></div>,
-//   ];
